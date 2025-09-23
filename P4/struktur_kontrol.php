c66fc20ea216e86dc1fcc3929670c45a1089bd70
Kamila Zahwa <kamilazhwa@gmail.com>
2025-09-23 21:25:14 +0700

    praktikum 4_bagian 5

--- P4/struktur_kontrol.php
+++ P4/struktur_kontrol.php
@@ -46,4 +46,16 @@ foreach ($skorUjian as $skor) {
 }
 echo "<br>";
 echo "Totak skor ujian adalah: $totalSkor";
+
+echo "<br>";
+$nilaiSiswa = [85, 92, 58, 64, 90, 55, 88, 79, 70, 96];
+
+echo "<br>";
+foreach ($nilaiSiswa as $nilai) {
+    if ($nilai<60) {
+        echo "Nilai: $nilai (Tidak lulus) <br>";
+        continue;
+    }
+    echo "Nilai: $nilai (Lulus) <br>";
+}
 ?>
