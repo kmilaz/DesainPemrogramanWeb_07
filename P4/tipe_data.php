389faf6cd30a154345934a7042562ab7e14c8470
Kamila Zahwa <kamilazhwa@gmail.com>
2025-09-23 09:57:32 +0700

    praktikum 2_bagian 5

--- P4/tipe_data.php
+++ P4/tipe_data.php
@@ -12,35 +12,45 @@ echo "Variabel d: {$d} <br>";
 echo "Variabel e: {$e} <br>";
 
 var_dump($e);
+echo "<br>";
 
 $nilaiMatematika=5.1;
 $nilaiIPA=6.7;
 $nilaiBahasaIndonesia=9.3;
 
 $rataRata = ($nilaiMatematika+$nilaiIPA+$nilaiBahasaIndonesia)/3;
-
+echo "<br>";
 echo "Matematika: {$nilaiMatematika} <br>";
 echo "IPA: {$nilaiIPA} <br>";
 echo "Bahasa Indonesia: {$nilaiBahasaIndonesia} <br>";
 echo "Rata-rata: {$rataRata} <br>";
 
 var_dump($rataRata);
+echo "<br>";
 
 $apakahSiswaLulus=true;
 $apakahSiswaSudahUjian=false;
-
+echo "<br>";
 var_dump($apakahSiswaLulus);
 echo "<br>";
 var_dump($apakahSiswaSudahUjian);
+
+echo "<br>";
 
 $namaDepan = "Ibnu";
 $namaBelakang = 'Jakaria';
 
 $namaLengkap = "{$namaDepan} {$namaBelakang}";
 $namaLengkap2 = $namaDepan . ' ' . $namaBelakang;
-
+echo "<br>";
 echo "Nama Depan: {$namaDepan} <br>";
 echo 'Nama Belakang: ' . $namaBelakang . '<br>';
 
 echo $namaLengkap;
+
+echo "<br>";
+
+$listMahasiswa = ["Wahid Abdullah", "Elmo Bachtiar", "Lendis Fabri"];
+echo "<br>";
+echo $listMahasiswa[0];
 ?>
