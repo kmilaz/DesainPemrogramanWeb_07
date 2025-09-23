cb6a7676d28b4dca78dc22e77693f65a3ccf1744
Kamila Zahwa <kamilazhwa@gmail.com>
2025-09-23 20:19:22 +0700

    praktikum 3_bagian 2

--- P4/operator.php
+++ P4/operator.php
@@ -15,3 +15,18 @@ echo "Hasil kali: {$hasilKali} <br>";
 echo "Hasil bagi: {$hasilBagi} <br>";
 echo "Sisa bagi: {$sisaBagi} <br>";
 echo "Hasil pangkat: {$pangkat} <br>";
+
+$hasilSama = $a===$b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a<$b;
+$hasilLebihBesar=$a>$b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "<br>";
+echo "Hasil sama: {$hasilSama} <br>";
+echo "Hasil tidak sama: {$hasilTidakSama} <br>";
+echo "Hasil lebih kecil: {$hasilLebihKecil} <br>";
+echo "Hasil lebih besar: {$hasilLebihBesar} <br>";
+echo "Hasil lebih kecil sama: {$hasilLebihKecilSama} <br>";
+echo "Hasil lebih besar sama: {$hasilLebihBesarSama} <br>";
