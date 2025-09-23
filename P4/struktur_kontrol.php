71b896ef4d48a717ecd8282cc1648ba2b6b937b4
Kamila Zahwa <kamilazhwa@gmail.com>
2025-09-23 21:20:41 +0700

    praktikum 4_bagian 4

--- P4/struktur_kontrol.php
+++ P4/struktur_kontrol.php
@@ -36,4 +36,14 @@ for ($i=1; $i <= $jumlahLahan; $i++) {
 }
 echo "<br>";
 echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
+
+echo "<br>";
+$skorUjian = [85, 92, 78, 96, 88];
+$totalSkor=0;
+
+foreach ($skorUjian as $skor) {
+    $totalSkor+=$skor;
+}
+echo "<br>";
+echo "Totak skor ujian adalah: $totalSkor";
 ?>
