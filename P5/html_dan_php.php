b154dc4db67487aed5406fba76d6cff7d926f2c1
Kamila Zahwa <kamilazhwa@gmail.com>
2025-09-30 21:22:41 +0700

    praktikum 8_bagian 2

--- P5/html_dan_php.php
+++ P5/html_dan_php.php
@@ -7,3 +7,13 @@
 <p>Tanggal Hari ini : <?php echo date("d M Y")?></p>
 </body>
 </html>
+
+<!-- Metode Kedua -->
+<?php
+echo '<html>';
+echo '<head><title>Cara02</title></head>'; 
+echo '<body>';
+echo '<p>Tanggal Hari ini : '.date('d M Y').'</p>';
+echo '</body>';
+echo '</html>';
+?>
