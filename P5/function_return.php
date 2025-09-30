c979582b056b270af1a14be59de540df29fe09e7
Kamila Zahwa <kamilazhwa@gmail.com>
2025-09-30 18:40:29 +0700

    praktikum 4_bagian 5

--- P5/function_return.php
+++ P5/function_return.php
@@ -3,7 +3,14 @@ function hitungUmur($thn_lahir, $thn_sekarang){
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
+function perkenalan($nama, $salam="Assalamualaikum"){
+    echo $salam.",";
+    echo "Perkenalan, nama saya ".$nama."<br/>";
 
-echo "Umur saya adalah ". hitungUmur(1988, 2023) ." tahun";
+    echo "Saya berusia ". hitungUmur(1988, 2023) ." tahun <br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+perkenalan("Elok");
 
 ?>
