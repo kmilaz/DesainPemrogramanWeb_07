8051c0b80e69ebba2e63a1465ec180940179f4d5
Kamila Zahwa <kamilazhwa@gmail.com>
2025-10-13 12:31:33 +0700

    praktikum 4_bagian 2

--- P7/html_aman.php
+++ P7/html_aman.php
@@ -1,4 +1,26 @@
-<?php
-$input = $_POST['input'];
-$input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulir Input Aman</title>
+</head>
+<body>
+    <h2>Masukkan Nama</h2>
+
+    <form method="post" action="html_aman.php">
+        <input type="text" name="input">
+        <input type="submit" name="submit" value="Kirim">
+    </form>
+    
+    <br>
+
+    <?php
+        if (isset($_POST['input'])) {
+            $input = $_POST['input'];
+            $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+            
+            echo "<b>Teks yang Anda masukkan (versi aman):</b><br>";
+            echo $input;
+        }
+    ?>
+</body>
+</html>
