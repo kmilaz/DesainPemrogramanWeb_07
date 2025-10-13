37dd61803d0e9986abd22b00f96d42c87795ed4b
Kamila Zahwa <kamilazhwa@gmail.com>
2025-10-13 16:22:48 +0700

    praktikum 5_soal 5.6

--- P7/regex.php
+++ P7/regex.php
@@ -24,7 +24,7 @@ $new_text = preg_replace($pattern, $replacement, $text);
 echo $new_text;
 
 echo "<br><br>";
-$pattern = '/go?d/';
+$pattern = '/go{2,2)d/';
 $text = 'god is good.';
 if (preg_match($pattern, $text, $matches)) {
     echo "Cocokkan: " . $matches[0];
