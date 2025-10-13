dddbb67efed0bb47caa4af63bb0f64682ac4323a
Kamila Zahwa <kamilazhwa@gmail.com>
2025-10-13 09:10:51 +0700

    praktikum 2_bagian 2

--- P7/empty.php
+++ P7/empty.php
@@ -5,4 +5,11 @@ if(empty($myArray)){
 } else {
     echo "Array terdefinisi dan tidak kosong.";
 }
+
+echo "<br>";
+if (empty($nonExistentVar)) {
+    echo "Variabel tidak terdefinisi atau kosong.";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong.";
+}
 ?>
