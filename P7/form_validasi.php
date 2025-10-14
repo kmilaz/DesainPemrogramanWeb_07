2df6a5989b9d1016d9d328d64f866b91683c7b69
Kamila Zahwa <kamilazhwa@gmail.com>
2025-10-14 16:54:25 +0700

    praktikum 7_soal 7.3

--- P7/form_validasi.php
+++ P7/form_validasi.php
@@ -2,10 +2,11 @@
 <html>
 <head>
     <title>Form Input dengan Validasi</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
     <h1>Form Input dengan Validasi</h1>
-    <form method="post" action="proses_validasi.php">
+    <form id="myForm">
         <label for="nama">Nama:</label>
         <input type="text" id="nama" name="nama">
         <span id="nama-error" style="color: red;"></span><br>
@@ -16,10 +17,14 @@
 
         <input type="submit" value="Submit">
     </form>
+
+    <div id="response-message"></div>
+
     
     <script>
         $(document).ready(function(){
             $("#myForm").submit(function(event){
+                event.preventDefault();
                 var nama = $("#nama").val();
                 var email = $("#email").val();
                 var valid = true;
@@ -38,8 +43,17 @@
                     $("#email-error").text("");
                 }
 
-                if (!valid) {
-                    event.preventDefault();
+                var formData = $("#myForm").serialize();
+
+                if (valid) {
+                    $.ajax({
+                        url: "proses_validasi.php",
+                        type: "POST",
+                        data: formData,
+                        success: function(response){
+                            $("#response-message").html(response);
+                        }
+                    });
                 }
             });
         });
