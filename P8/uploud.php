f815207efe0566ada360f7047a332e45da3aa197
Kamila Zahwa <kamilazhwa@gmail.com>
2025-10-21 22:35:47 +0700

    praktikum 1_pertanyaan 7

--- P8/uploud.php
+++ P8/uploud.php
@@ -4,20 +4,17 @@ if (isset($_POST["submit"])) {
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-    $maxsize = 5*1024*1024;
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
+    $maxsize = 3*1024*1024;
 
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"]<=$maxsize) {
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
             echo "File berhasil diunggah.";
-            echo "<h3>Thumbnail:</h3>";
-            echo "<img src='$targetfile' width='200' alt='Thumbnail Gambar'>";
         } else {
             echo "Gagal mengunggah file.";
         }
     } else {
         echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
     }
-    
 }
 ?>
