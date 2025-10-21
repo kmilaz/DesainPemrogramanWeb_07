1daa6949c99a2c950896419f1a637e3579af2232
Kamila Zahwa <kamilazhwa@gmail.com>
2025-10-21 22:10:49 +0700

    praktikum 1_bagian 1

---
+++ P8/form_uploud.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>File Uploud</title>
+    </head>
+    <body>
+        <form action="uploud.php" method="POST" enctype="multipart/form-data">
+            <input type="file" name="myfile">
+            <input type="submit" name="submit">
+        </form>
+    </body>
+</html>
