4253cbefe5b432b21db89fe7c29da7ecdb11ae8d
Kamila Zahwa <kamilazhwa@gmail.com>
2025-10-22 04:26:38 +0700

    praktikum 4

--- P8/upload.js
+++ P8/upload.js
@@ -1,19 +1,27 @@
 $(document).ready(function(){
-    $('#upload-form').submit(function(e){
-        e.preventDefault(); 
-        var formData = new FormData(this);
+    $('#file').change(function(){ 
+        if (this.files.length > 0) { 
+            $('#upload-button').prop('disabled', false).css('opacity', 1); 
+        } else {
+            $('#upload-button').prop('disabled', true).css('opacity', 0.5); 
+        }
+    });
 
-        $.ajax({
+    $('#upload-form').submit(function(e){ 
+        e.preventDefault(); 
+        var formData = new FormData(this); 
+
+        $.ajax({ 
             type: 'POST',
-            url: 'upload_ajax.php',
-            data: formData,
-            cache: false,
-            contentType: false,
-            processData: false,
-            success: function(response) {
+            url: 'upload_ajax.php', 
+            data: formData, 
+            cache: false, 
+            contentType: false, 
+            processData: false, 
+            success: function(response) { 
                 $('#status').html(response); 
             },
-            error: function(){
+            error: function(){ 
                 $('#status').html('Terjadi kesalahan saat mengunggah file.'); 
             }
         });
