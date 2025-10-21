106b715b85174dc70ba2deaa8bab11516d67709f
Kamila Zahwa <kamilazhwa@gmail.com>
2025-10-21 22:29:06 +0700

    praktikum 1_pertanyaan 5

--- P8/uploud.php
+++ P8/uploud.php
@@ -10,6 +10,8 @@ if (isset($_POST["submit"])) {
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"]<=$maxsize) {
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
             echo "File berhasil diunggah.";
+            echo "<h3>Thumbnail:</h3>";
+            echo "<img src='$targetfile' width='200' alt='Thumbnail Gambar'>";
         } else {
             echo "Gagal mengunggah file.";
         }
