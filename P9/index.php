3e9b12673bf7cccf7c09458895b351189ef7f2da
Kamila Zahwa <kamilazhwa@gmail.com>
2025-10-29 00:00:27 +0700

    praktikum 1_soal 3

--- P9/index.php
+++ P9/index.php
@@ -3,5 +3,8 @@ include 'koneksi.php';
 
 $createUserTableQuery = 'CREATE TABLE user(id INT(11) AUTO_INCREMENT PRIMARY KEY, 
                         username VARCHAR(50), password VARCHAR(50));';
-mysqli_query($koneksi, $createUserTableQuery);
+//mysqli_query($koneksi, $createUserTableQuery);
+
+$insertDataUser = "INSERT INTO user(id, username, password) VALUES('1', 'admin', MD5('123'));";
+mysqli_query($koneksi, $insertDataUser);
 ?>
